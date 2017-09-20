c022b512c1ff9c7608c20f03392a8bdab83638b3
Nathan Struhs <nathanstruhs@gmail.com>
2017-09-20 16:49:31 -0700

    CHALLENGE: adjust gravity of correct/incorrect toasts

--- app/src/main/java/com/nathanstruhs/geoquiz/QuizActivity.java
+++ app/src/main/java/com/nathanstruhs/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.nathanstruhs.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -30,14 +31,18 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                Toast toast = Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP| Gravity.CENTER_HORIZONTAL, 0, 0);
+                toast.show();
             }
         });
 
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                Toast toast = Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP| Gravity.CENTER_HORIZONTAL, 0, 0);
+                toast.show();
             }
         });
     }
