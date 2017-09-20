86b566f019efaed41561ef81cac3f13ceebff4b6
Nathan Struhs <nathanstruhs@gmail.com>
2017-09-20 15:46:51 -0700

    Set up listeners for true and false buttons
    All listeners within the book use anonymous inner classes. This ensures
    that the implementation of the listener is right where you want to see
    them. This way you do not need another class located in another file.

--- app/src/main/java/com/nathanstruhs/geoquiz/QuizActivity.java
+++ app/src/main/java/com/nathanstruhs/geoquiz/QuizActivity.java
@@ -2,16 +2,42 @@ package com.nathanstruhs.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
+
+    // button member variables
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     // onCreate() called when an instance of the activity subclass is created
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         // for the activity to receive a user interface
         // setContentView() 'inflates' a layout and puts it on screen
             // each widget int he layout is instantiated as defined by its attributes
         setContentView(R.layout.activity_quiz);
+
+        // retrieving the resource id to connect with our QuizActivity member variables
+            // (Button) type casts the view to a Button object
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // does nothing yet!
+            }
+        });
+
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // does nothing yet!
+            }
+        });
     }
 }
