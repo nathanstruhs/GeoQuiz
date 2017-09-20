b499c7307782aa063da78ecbe9b6ae40c46a680d
Nathan Struhs <nathanstruhs@gmail.com>
2017-09-20 15:28:55 -0700

    Create resource IDs for the true and false buttons

--- app/src/main/java/com/nathanstruhs/geoquiz/QuizActivity.java
+++ app/src/main/java/com/nathanstruhs/geoquiz/QuizActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 public class QuizActivity extends AppCompatActivity {
 
+    // onCreate() called when an instance of the activity subclass is created
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // for the activity to receive a user interface
+        // setContentView() 'inflates' a layout and puts it on screen
+            // each widget int he layout is instantiated as defined by its attributes
         setContentView(R.layout.activity_quiz);
     }
 }
