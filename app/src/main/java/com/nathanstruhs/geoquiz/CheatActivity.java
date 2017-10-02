93e6e585cfded37d6eb27c94c4be5108f659eb4f
Nathan Struhs <nathanstruhs@gmail.com>
2017-10-02 11:35:24 -0700

    checkAnswer() typo

--- app/src/main/java/com/nathanstruhs/geoquiz/CheatActivity.java
+++ app/src/main/java/com/nathanstruhs/geoquiz/CheatActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,6 +18,9 @@ public class CheatActivity extends AppCompatActivity {
             "com.nathanstruhs.geoquiz.answer_shown";
 
     private boolean mAnswerIsTrue;
+
+    public static final String KEY_CHEAT = "cheat";
+    private boolean mHasCheated;
 
     private TextView mAnswerTextView;
     private Button mShowAnswerButton;
@@ -35,6 +39,10 @@ public class CheatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
 
+        if (savedInstanceState != null) {
+            mHasCheated = savedInstanceState.getBoolean(KEY_CHEAT, false);
+        }
+
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
 
         mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
@@ -48,14 +56,21 @@ public class CheatActivity extends AppCompatActivity {
                 } else {
                     mAnswerTextView.setText(R.string.false_button);
                 }
+
                 setAnswerShownResult(true);
             }
         });
-    };git
+    };
 
     private void setAnswerShownResult(boolean isAnswerShown) {
         Intent data = new Intent();
         data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
         setResult(RESULT_OK, data);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putBoolean(KEY_CHEAT, mHasCheated);
     }
 }
