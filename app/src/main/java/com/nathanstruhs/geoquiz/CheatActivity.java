460d743fcacdc0f1732e87aed6d3c903008fcc7e
Nathan Struhs <nathanstruhs@gmail.com>
2017-10-05 17:07:51 -0700

    Ch 6, pg 119: wrap new version methods in if
    conditional. To ensure backwards compatibility.

--- app/src/main/java/com/nathanstruhs/geoquiz/CheatActivity.java
+++ app/src/main/java/com/nathanstruhs/geoquiz/CheatActivity.java
@@ -4,6 +4,7 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -53,18 +54,23 @@ public class CheatActivity extends AppCompatActivity {
                 }
                 setAnswerShownResult(true);
 
-                int cx = mShowAnswerButton.getWidth() /2;
-                int cy = mShowAnswerButton.getHeight() / 2;
-                float radius = mShowAnswerButton.getWidth();
-                Animator anim = ViewAnimationUtils
-                        .createCircularReveal(mShowAnswerButton, cx, cy, radius, 0);
-                anim.addListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        super.onAnimationEnd(animation);
-                        mShowAnswerButton.setVisibility(View.INVISIBLE);
-                    }
-                });
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswerButton.getWidth() / 2;
+                    int cy = mShowAnswerButton.getHeight() / 2;
+                    float radius = mShowAnswerButton.getWidth();
+                    Animator anim = ViewAnimationUtils
+                            .createCircularReveal(mShowAnswerButton, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mShowAnswerButton.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    anim.start();
+                } else {
+                    mShowAnswerButton.setVisibility(View.INVISIBLE);
+                }
             }
         });
     }
